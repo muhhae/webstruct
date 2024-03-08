02f4e0a6a963ae68c2d468310950aa9cbdf5238b
muhhae <haekal.muhamat@gmail.com>
2024-03-08 11:48:01 +0700

    proper element

--- element/basic.go
+++ element/basic.go
@@ -43,23 +43,57 @@ func (d Div) Html() string {
 }
 
 type Span struct {
-	Div
+	Id               string
+	Style            webtype.Style
+	Class            webtype.Class
+	CustomAttributes webtype.Attribute
+	Children         webtype.Children
+}
+
+func (s Span) ToCustomElement() CustomElement {
+	return CustomElement{
+		Tag: "span",
+		Attributes: util.Combine(
+			s.CustomAttributes,
+			webtype.Attribute{
+				"id":    s.Id,
+				"style": util.StyleFormat(s.Style),
+				"class": util.ListToString(s.Class),
+			},
+		),
+		Children: s.Children,
+	}
 }
 
 func (s Span) Html() string {
-	e := s.ToCustomElement()
-	e.Tag = "span"
-	return e.Html()
+	return s.ToCustomElement().Html()
 }
 
 type P struct {
-	Div
+	Id               string
+	Style            webtype.Style
+	Class            webtype.Class
+	CustomAttributes webtype.Attribute
+	Children         webtype.Children
+}
+
+func (p P) ToCustomElement() CustomElement {
+	return CustomElement{
+		Tag: "p",
+		Attributes: util.Combine(
+			p.CustomAttributes,
+			webtype.Attribute{
+				"id":    p.Id,
+				"style": util.StyleFormat(p.Style),
+				"class": util.ListToString(p.Class),
+			},
+		),
+		Children: p.Children,
+	}
 }
 
 func (p P) Html() string {
-	e := p.ToCustomElement()
-	e.Tag = "p"
-	return e.Html()
+	return p.ToCustomElement().Html()
 }
 
 type RawText string
@@ -69,43 +103,108 @@ func (t RawText) Html() string {
 }
 
 type A struct {
-	Div
-	Href string
+	Id               string
+	Style            webtype.Style
+	Class            webtype.Class
+	CustomAttributes webtype.Attribute
+	Children         webtype.Children
+	Href             string
+}
+
+func (a A) ToCustomElement() CustomElement {
+	return CustomElement{
+		Tag: "a",
+		Attributes: util.Combine(
+			a.CustomAttributes,
+			webtype.Attribute{
+				"id":    a.Id,
+				"style": util.StyleFormat(a.Style),
+				"class": util.ListToString(a.Class),
+				"href":  a.Href,
+			},
+		),
+		Children: a.Children,
+	}
 }
 
 func (a A) Html() string {
-	e := a.ToCustomElement()
-	e.Tag = "a"
-	e.Attributes["href"] = a.Href
-	return a.Html()
+	return a.ToCustomElement().Html()
 }
 
 type H1 struct {
-	Div
+	Id               string
+	Style            webtype.Style
+	Class            webtype.Class
+	CustomAttributes webtype.Attribute
+	Children         webtype.Children
 }
 
+func (h1 H1) ToCustomElement() CustomElement {
+	return CustomElement{
+		Tag: "h1.v",
+		Attributes: util.Combine(
+			h1.CustomAttributes,
+			webtype.Attribute{
+				"id":    h1.Id,
+				"style": util.StyleFormat(h1.Style),
+				"class": util.ListToString(h1.Class),
+			},
+		),
+		Children: h1.Children,
+	}
+}
 func (h1 H1) Html() string {
-	e := h1.ToCustomElement()
-	e.Tag = "h1"
-	return e.Html()
+	return h1.ToCustomElement().Html()
 }
 
 type H2 struct {
-	Div
+	Id               string
+	Style            webtype.Style
+	Class            webtype.Class
+	CustomAttributes webtype.Attribute
+	Children         webtype.Children
 }
 
+func (h2 H2) ToCustomElement() CustomElement {
+	return CustomElement{
+		Tag: "h2.v",
+		Attributes: util.Combine(
+			h2.CustomAttributes,
+			webtype.Attribute{
+				"id":    h2.Id,
+				"style": util.StyleFormat(h2.Style),
+				"class": util.ListToString(h2.Class),
+			},
+		),
+		Children: h2.Children,
+	}
+}
 func (h2 H2) Html() string {
-	e := h2.ToCustomElement()
-	e.Tag = "h2"
-	return e.Html()
+	return h2.ToCustomElement().Html()
 }
 
 type H3 struct {
-	Div
+	Id               string
+	Style            webtype.Style
+	Class            webtype.Class
+	CustomAttributes webtype.Attribute
+	Children         webtype.Children
 }
 
-func (h3 H3) Html() string {
-	e := h3.ToCustomElement()
-	e.Tag = "h3"
-	return e.Html()
+func (h2 H3) ToCustomElement() CustomElement {
+	return CustomElement{
+		Tag: "h2.v",
+		Attributes: util.Combine(
+			h2.CustomAttributes,
+			webtype.Attribute{
+				"id":    h2.Id,
+				"style": util.StyleFormat(h2.Style),
+				"class": util.ListToString(h2.Class),
+			},
+		),
+		Children: h2.Children,
+	}
+}
+func (h2 H3) Html() string {
+	return h2.ToCustomElement().Html()
 }
